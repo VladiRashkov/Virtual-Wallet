b78cc150e8bfdb300975e3b72407c836fd8e6931
Vladislav <vladislav.rashkoff@gmail.com>
2024-05-20 21:15:36 +0300

    test

--- routers/transactions.py
+++ routers/transactions.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, Query
 from services import transactions_services
 from common.authorization import get_current_user
-from typing import Optional
+from typing import Optional, List, Dict, Any
 
 # 1. Transaction history after a regular user has logged in  -- sending part, reciever part in progress
 # ->admin cannot check the transaction history!
@@ -11,8 +11,8 @@ from typing import Optional
 # 5. Add money to wallet
 
 # 6. Filter by date, sender, recipient, and direction (in/out)
-# 7. Sort by date or amount
-# 8. Withdraw
+# 7. Sort by date or amount -- DONE
+# 8. Withdraw -- DONE
 
 transaction_router = APIRouter(prefix='/transactions')
 
@@ -20,15 +20,24 @@ transaction_router = APIRouter(prefix='/transactions')
 @transaction_router.get('/')  # will require token of the user
 def get_transactions(sort_by: Optional[str] = Query(None, pattern='^(created_at|amount)$'),
                      order: Optional[str] = Query(None, pattern='^(asc|desc)$'),
+                     transaction_type:str = Query('all', pattern='^(sent|received|all)'),
                      sender_id: int = Depends(get_current_user)):
-    transactions = transactions_services.all_user_transactions(sender_id, sort_by,order)
+    transactions = transactions_services.all_user_transactions(sender_id, transaction_type, sort_by, order)
     return transactions
+
+# @transaction_router.get('/filter')
+# def get_filter(date:str,
+#                receiver_id:int,
+#                transaction_type:str = Query('all', pattern='^(sent|received)',
+#                sender_id: int = Depends(get_current_user)):
+    
+    
 
 
 @transaction_router.post('/create_transaction')
 def create_transaction(receiver_id: int, amount: float, category:str, sender_id: int = Depends(get_current_user)):
     result = transactions_services.transfer_money(sender_id, receiver_id, amount, category)
-    return {'message': result}
+    return {'message': result[0]}
 
 @transaction_router.put('/add')
 def add_money(deposit:float, user: int=Depends(get_current_user)):
