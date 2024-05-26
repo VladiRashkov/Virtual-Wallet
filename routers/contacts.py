03afba919abdebedbfb2f3cfa530216da2808371
Vladislav <vladislav.rashkoff@gmail.com>
2024-05-26 19:07:53 +0300

    search contact by username, email or telephone

--- routers/contacts.py
+++ routers/contacts.py
@@ -1,11 +1,21 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status
 from common.authorization import get_current_user
-from services.contacts_services import get_contacts_of_user
+from services.contacts_services import get_contacts_of_user, find_contact_by_attribute
 
 router_contacts = APIRouter(prefix='/contacts')
 
 
 @router_contacts.get('/')
 def all_users_contacts(logged_user_id: int = Depends(get_current_user)): 
+    '''Retrieve contact details for a given user.'''
     result = get_contacts_of_user(logged_user_id)
     return result
+
+@router_contacts.get('/find')
+def find_contact(logged_user_id:int = Depends(get_current_user), search: str = '', value: str = ''):
+    '''Find a user's contact by a specific attribute (phone number, username, or email).'''
+    contact = find_contact_by_attribute(logged_user_id, search, value)
+    if contact:
+        return contact
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Contact not found.')
--- services/contacts_services.py
+++ services/contacts_services.py
@@ -19,3 +19,29 @@ def get_contacts_of_user(logged_user_id:int):
     else: 
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='No contacts found!')
     
+    
+def find_contact_by_attribute(logged_user_id: int, search_by_criteria: str, value: str):
+    '''Find a user's contact by a specific attribute (phone number, username, or email).'''
+    # Map the attribute to the corresponding column name in the 'users' table
+    column_map = {
+        'phone_number': 'phone_number',
+        'username': 'username',
+        'email': 'email'
+    }
+    column_name = column_map.get(search_by_criteria)
+    if column_name:
+        # Search for the contact using the specified attribute and value
+        contact = query.table('contacts') \
+            .select('contact_name_id') \
+            .eq('current_user', logged_user_id) \
+            .execute()
+        
+        if contact.data:
+            contact_name_ids = [c['contact_name_id'] for c in contact.data]
+            user = query.table('users') \
+                .select('id', 'username', 'email', 'phone_number') \
+                .in_('id', contact_name_ids) \
+                .eq(column_name, value) \
+                .execute().data
+            return user[0] if user else None
+    return None
