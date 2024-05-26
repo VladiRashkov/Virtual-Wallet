4ba4e6ee5c099b042759af0eb0d1fafac79c4add
Vladislav <vladislav.rashkoff@gmail.com>
2024-05-26 17:23:00 +0300

    çontacts list updated

--- routers/contacts.py
+++ routers/contacts.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter, Depends, HTTPException
-from data.models import Contacts
 from common.authorization import get_current_user
 from services.contacts_services import get_contacts_of_user
 
@@ -9,4 +8,4 @@ router_contacts = APIRouter(prefix='/contacts')
 @router_contacts.get('/')
 def all_users_contacts(logged_user_id: int = Depends(get_current_user)): 
     result = get_contacts_of_user(logged_user_id)
-    return result.data
+    return result
